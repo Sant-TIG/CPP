d4699b6cda0a509248aff845b00b1ef13d84a0a7
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2023-08-30 16:06:58 +0200

    Update Phonebook.hpp

--- CPP00/ex01/Phonebook.hpp
+++ CPP00/ex01/Phonebook.hpp
@@ -20,17 +20,17 @@
 class Phonebook {
 	private:
 		static const int	MAX_CONTACTS = 8;
-		Contact				contacts[MAX_CONTACTS];
-		static int			nbrContacts;
-		static int			currContact;
+		Contact			contacts[MAX_CONTACTS];
+		static int		nbrContacts;
+		static int		currContact;
 
 	public:
 		Phonebook( void );
 		Phonebook( const Phonebook &onePhonebook );
 		Phonebook & operator=( const Phonebook &onePhonebook );
 		~Phonebook( void );
-		int			getNbrContacts ( void ) const;
-		int			getCurrContact ( void ) const;
+		int		getNbrContacts ( void ) const;
+		int		getCurrContact ( void ) const;
 		std::string	getContactFirstName( int i ) const;
 		std::string	getContactLastName( int i ) const;
 		std::string	getContactNickname( int i ) const;
@@ -38,8 +38,8 @@ class Phonebook {
 		std::string	getContactSecret( int i ) const;
 		std::string	truncString( const std::string &str ) const;
 		void		addContact( const std::string &fName, const std::string &lName,
-								const std::string &nName, const std::string &phone,
-								const std::string &secret );
+					    const std::string &nName, const std::string &phone,
+					    const std::string &secret );
 		void		showContacts( void ) const;
 		void		showOneContact( int i ) const;
 };
