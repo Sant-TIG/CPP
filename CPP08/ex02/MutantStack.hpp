46ef0af53170e87606b7693627b59c087246184b
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2024-08-12 12:37:21 +0200

    Update MutantStack.hpp

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -32,13 +32,13 @@ class MutantStack : public std::stack<T, Container> {
 		typedef typename Container::reverse_iterator		reverse_iterator;
 		typedef typename Container::const_reverse_iterator	const_reverse_iterator;
 
-		iterator				begin( void );
-		iterator				end( void );
-		const_iterator			begin( void ) const;
-		const_iterator			end( void ) const;
+		iterator		begin( void );
+		iterator		end( void );
+		const_iterator		begin( void ) const;
+		const_iterator		end( void ) const;
 
-		reverse_iterator		rbegin( void );
-		reverse_iterator		rend( void );
+		reverse_iterator	rbegin( void );
+		reverse_iterator	rend( void );
 		const_reverse_iterator	rbegin( void ) const;
 		const_reverse_iterator	rend( void ) const;
 };
